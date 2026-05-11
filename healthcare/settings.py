a7535013d41868183d52e4667faa65581936f621
anshika joshi <anshikajoshi1823@gmail.com>
2026-05-11 11:43:23 +0530

    Fix Django auth signup/signin on Render

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -78,10 +78,15 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+if os.getenv('RENDER') == '1':
+    DATABASE_NAME = os.getenv('DB_PATH', '/var/data/db.sqlite3')
+else:
+    DATABASE_NAME = os.getenv('DB_PATH', BASE_DIR / 'db.sqlite3')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/var/data/db.sqlite3',
+        'NAME': DATABASE_NAME,
     }
 }
 
