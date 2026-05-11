886e7f3d467c168f3cbadf46d18e8529a357dd63
anshika joshi <anshikajoshi1823@gmail.com>
2026-05-11 22:12:11 +0530

    Fix Render migration startup crash

--- healthcare/wsgi.py
+++ healthcare/wsgi.py
@@ -8,4 +8,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthcare.settings')
 
+print('Startup log: Django DB path = /var/data/db.sqlite3')
+
 application = get_wsgi_application()
