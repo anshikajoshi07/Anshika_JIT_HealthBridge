1e712e1b67adc528f15020b8850ddc5067e82a20
anshika joshi <anshikajoshi1823@gmail.com>
2026-05-12 15:42:53 +0530

    Fix media serving for production and clean signup options

--- healthcare/urls.py
+++ healthcare/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('', include('health.urls')),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
